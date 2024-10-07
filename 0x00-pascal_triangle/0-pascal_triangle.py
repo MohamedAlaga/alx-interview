b10603b92b571f74c281e9db9a06839b764a9c20
MohamedAlaga <malaga.142014@gmail.com>
2024-10-07 04:05:12 +0300

    first attempt

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,9 @@
+def pascal_triangle(n):
+    if (n <= 0):
+        return []
+    totalList = []
+    for i in range(n) :
+        x = str(pow(11,i))
+        totalList.append(list(x))
+        print(x)
+    return totalList
